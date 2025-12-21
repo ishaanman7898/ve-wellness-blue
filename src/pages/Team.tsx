8807f196251fc2ae7a7d5a86414216c068b55d3d
MeridianAlgo <meridianalgo@gmail.com>
2025-12-21 17:19:38 -0600

    Fix Team page: add missing useState import

--- src/pages/Team.tsx
+++ src/pages/Team.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Navbar } from "@/components/Navbar";
 import { Footer } from "@/components/Footer";
 import { CustomCursor } from "@/components/CustomCursor";
